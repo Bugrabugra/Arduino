d70a13e0075c62764285bcddf592e219dc352f0f
bugrabugra <bugraotken@gmail.com>
2019-09-24 17:58:46 +0300

    Wireless

--- Projeler/Wireless/Long_wifi_transrec1/Long_wifi_transrec1.ino
+++ Projeler/Wireless/Long_wifi_transrec1/Long_wifi_transrec1.ino
@@ -1,46 +1,80 @@
+#include "Arduino.h"
 #include <SPI.h>
-#include <nRF24L01.h>
 #include <RF24.h>
 
-RF24 radio(7, 8);  // CE, CSN
+// This is just the way the RF24 library works:
+// Hardware configuration: Set up nRF24L01 radio on SPI bus (pins 10, 11, 12, 13) plus pins 7 & 8
+RF24 radio(7, 8);
 
-const byte address[][6] = {"00001", "00002"};
-boolean stateButton1 = 0;
-boolean stateButton2 = 0;
+int button = 3;
+int led = 4;
 
-int pinButton = 2;
-int pinLED = 9;
+byte addresses[][6] = {"1Node","2Node"};
 
-void setup()
-{
+// -----------------------------------------------------------------------------
+// SETUP   SETUP   SETUP   SETUP   SETUP   SETUP   SETUP   SETUP   SETUP
+// -----------------------------------------------------------------------------
+void setup() {
+  Serial.begin(9600);
+  Serial.println("THIS IS THE RECEIVER CODE - YOU NEED THE OTHER ARDUINO TO TRANSMIT");
+  pinMode(button, INPUT_PULLUP);
+  pinMode(led, OUTPUT);
+  // Initiate the radio object
   radio.begin();
-  radio.openWritingPipe(address[0]);
-  radio.openReadingPipe(1, address[1]);
-  pinMode(pinButton, INPUT_PULLUP);
-  pinMode(pinLED, OUTPUT);
 
-}
-void loop()
-{
-  //Yazma
-  delay(5);
-  radio.stopListening();
-  stateButton1 = digitalRead(pinButton);
-  radio.write(&stateButton1, sizeof(stateButton1));
-  
-  delay(5);
-  //Okuma
+  // Set the transmit power to lowest available to prevent power supply related issues
+  radio.setPALevel(RF24_PA_MIN);
+
+  // Set the speed of the transmission to the quickest available
+  radio.setDataRate(RF24_2MBPS);
+
+  // Use a channel unlikely to be used by Wifi, Microwave ovens etc
+  radio.setChannel(124);
+
+  // Open a writing and reading pipe on each radio, with opposite addresses
+  radio.openWritingPipe(addresses[0]);
+  radio.openReadingPipe(1, addresses[1]);
+
+  // Start the radio listening for data
   radio.startListening();
-  if (radio.available())
-  {
-    radio.read(&stateButton2, sizeof(stateButton2));
-    if (stateButton2 == LOW)
+}
+
+// -----------------------------------------------------------------------------
+// We are LISTENING on this device only (although we do transmit a response)
+// -----------------------------------------------------------------------------
+void loop() {
+
+  // This is what we receive from the other device (the transmitter)
+  int dataRx;
+
+  // Is there any data for us to get?
+  if ( radio.available()) {
+
+    // Go and read the data and put it into that variable
+    while (radio.available()) {
+      radio.read( &dataRx, sizeof(dataRx));
+    }
+    if (dataRx == 0)
     {
-      digitalWrite(pinLED, HIGH);
+      digitalWrite(led, HIGH);
     }
     else
     {
-      digitalWrite(pinLED, LOW);
-    } 
+      digitalWrite(led, LOW);
+    }
+    // No more data to get so send it back but add 1 first just for kicks
+    // First, stop listening so we can talk
+    radio.stopListening();
+    int dataTx = digitalRead(button);
+    radio.write( &dataTx, sizeof(dataTx) );
+
+    // Now, resume listening so we catch the next packets.
+    radio.startListening();
+
+    // Tell the user what we sent back (the random numer + 1)
+    Serial.print("Sent: ");
+    Serial.print(dataTx);
+    Serial.print(", received: ");
+    Serial.println(dataRx);
   }
 }
--- Projeler/Wireless/Long_wifi_transrec2/Long_wifi_transrec2.ino
+++ Projeler/Wireless/Long_wifi_transrec2/Long_wifi_transrec2.ino
@@ -1,45 +1,93 @@
+#include "Arduino.h"
 #include <SPI.h>
-#include <nRF24L01.h>
 #include <RF24.h>
 
-RF24 radio(7, 8);  // CE, CSN
+// This is just the way the RF24 library works:
+// Hardware configuration: Set up nRF24L01 radio on SPI bus (pins 10, 11, 12, 13) plus pins 7 & 8
+RF24 radio(7, 8);
 
-const byte address[][6] = {"00001", "00002"};
-boolean stateButton1 = 0;
-boolean stateButton2 = 0;
+int button = 3;
+int led = 4;
 
-int pinButton = 2;
-int pinLED = 9;
+byte addresses[][6] = {"1Node", "2Node"};
 
-void setup()
-{
+// -----------------------------------------------------------------------------
+// SETUP   SETUP   SETUP   SETUP   SETUP   SETUP   SETUP   SETUP   SETUP
+// -----------------------------------------------------------------------------
+void setup() {
+  Serial.begin(9600);
+  Serial.println("THIS IS THE TRANSMITTER CODE - YOU NEED THE OTHER ARDIUNO TO SEND BACK A RESPONSE");
+  pinMode(button, INPUT_PULLUP);
+  pinMode(led, OUTPUT);
+  // Initiate the radio object
   radio.begin();
-  radio.openWritingPipe(address[1]);
-  radio.openReadingPipe(1, address[0]);
-  pinMode(pinButton, INPUT_PULLUP);
-  pinMode(pinLED, OUTPUT); 
+
+  // Set the transmit power to lowest available to prevent power supply related issues
+  radio.setPALevel(RF24_PA_MIN);
+
+  // Set the speed of the transmission to the quickest available
+  radio.setDataRate(RF24_2MBPS);
+
+  // Use a channel unlikely to be used by Wifi, Microwave ovens etc
+  radio.setChannel(124);
+
+  // Open a writing and reading pipe on each radio, with opposite addresses
+  radio.openWritingPipe(addresses[1]);
+  radio.openReadingPipe(1, addresses[0]);
+
+  // Random number seeding (we're going to be sending a single random number)
 }
 
-void loop()
-{
-  //Okuma
-  delay(5);
-  radio.startListening();
-  if (radio.available())
-  {
-    radio.read(&stateButton1, sizeof(stateButton1));
-    if (stateButton1 == LOW)
-    {
-      digitalWrite(pinLED, HIGH);
-    }
-    else
-    {
-      digitalWrite(pinLED, LOW);
-    } 
+// -----------------------------------------------------------------------------
+// LOOP     LOOP     LOOP     LOOP     LOOP     LOOP     LOOP     LOOP     LOOP
+// -----------------------------------------------------------------------------
+void loop() {
+
+  // Generate a single random character to transmit
+  int dataTx = digitalRead(button);
+    
+  // Ensure we have stopped listening (even if we're not) or we won't be able to transmit
+  radio.stopListening(); 
+
+  // Did we manage to SUCCESSFULLY transmit that (by getting an acknowledgement back from the other Arduino)?
+  // Even we didn't we'll continue with the sketch, you never know, the radio fairies may help us
+  if (!radio.write( &dataTx, sizeof(dataTx) )) {
+    Serial.println("No acknowledgement of transmission - receiving radio device connected?");    
   }
-  //Yazma
-  delay(5);
-  radio.stopListening();
-  stateButton2 = digitalRead(pinButton);
-  radio.write(&stateButton2, sizeof(stateButton2));
+
+  // Now listen for a response
+  radio.startListening();
+  
+  // But we won't listen for long, 200 milliseconds is enough
+  unsigned long started_waiting_at = millis();
+
+  // Loop here until we get indication that some data is ready for us to read (or we time out)
+  while ( ! radio.available() ) {
+
+    // Oh dear, no response received within our timescale
+    if (millis() - started_waiting_at > 200 ) {
+      Serial.println("No response received - timeout!");
+      return;
+    }
+  }
+
+  // Now read the data that is waiting for us in the nRF24L01's buffer
+  int dataRx;
+  radio.read( &dataRx, sizeof(dataRx) );
+  if (dataRx == 0)
+  {
+    digitalWrite(led, HIGH);
+  }
+  else
+  {
+    digitalWrite(led, LOW);
+  }
+  // Show user what we sent and what we got back
+  Serial.print("Sent: ");
+  Serial.print(dataTx);
+  Serial.print(", received: ");
+  Serial.println(dataRx);
+
+  // Try again 1s later
+  delay(1000);
 }
