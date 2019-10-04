df6c59f48bbb8562f9a2fce3405ef2e32407bbbb
bugrabugra <bugraotken@gmail.com>
2019-10-04 18:20:23 +0300

    Devam wifi

--- Projeler/WIFI/WIFI_Deneme/WIFI_Deneme.ino
+++ Projeler/WIFI/WIFI_Deneme/WIFI_Deneme.ino
@@ -1,160 +1,19 @@
-//ESP TX >> Mega RX(0)
-//ESP RX >> Mega TX(0)
-
-#include <ArduinoJson.h>
-
-// #define ag_ismi "KAT3"
-// #define ag_sifresi "UnV-2019!Wf++"
-#define ag_ismi "SONRASI_YOKTU"
-#define ag_sifresi "BuuRA03045025"
-
-
-#define host "184.106.153.149"
-int sayi = 0;
+char bir[23] = "AT+CIPSTART=\"TCP\",\"";
+char iki[19] = "api.thingspeak.com";
+char birlesmis[48] = "";
 
 void setup()
 {
-  Serial.begin(115200); //Seriport'u açıyoruz. Güncellediğimiz
-
-  Serial.println("AT");
-  delay(5000);
-
-  if (Serial.find("OK"))
-  {
-    //connect to your wifi netowork
-    startWifi();
-  } 
+  Serial.begin(9600);
+  strcat(birlesmis, bir);
+  strcat(birlesmis, iki);
+  char uc[6] = "\",80";
+  strcat(birlesmis, uc);
 }
 
-void loop() 
+void loop()
 {
-  httpRequest();
-  delay(10000);
-}
-
-void httpRequest() 
-{
-  String start_com = "AT+CIPSTART=\"TCP\",\"";
-  start_com += host;
-  start_com += "\",80";
-
-  Serial.println(start_com);  //AT+CIPSTART komutu ile sunucuya bağlanmak için sunucudan izin istiyoruz.
-  delay(2000);
-
-  if (Serial.find("Error"))
-  {
-    return false;
-  }
-
-  sayi++;
-  String postData = "GET /channels/871881/feeds.json?results=1 HTTP/1.1\r\n";
-  postData += "Host: api.thingspeak.com\r\n";
-  postData += "Connection: keep-alive\r\n";
-  postData += "\r\n";
-  //TCP burada yapacağımız bağlantı çeşidini gösteriyor. 80 ise bağlanacağımız portu gösteriyor
-
-  //veri yollayacağımız zaman bu komutu kullanıyoruz. Bu komut ile önce kaç tane karakter yollayacağımızı söylememiz gerekiyor.
-  Serial.print("AT+CIPSEND="); 
-  Serial.println(postData.length());
-
-  //eğer sunucu ile iletişim sağlayıp komut uzunluğunu gönderebilmişsek ESP modülü bize ">" işareti ile geri dönüyor.
-  if (Serial.find(">")) {
-        // arduino da ">" işaretini gördüğü anda sıcaklık verisini esp modülü ile thingspeak sunucusuna yolluyor.
-        Serial.print(postData);
-        // delay(2000); //Delayi önce koyunca veriyi çekemedi
-        gelenVeri();
-        delay(3000);
-        Serial.println("AT+CIPCLOSE");
-
-        // çalışma sıklığı saniye olarak
-    //delay(device.interval * 1000);
-        delay(2500);
-
-  }
-}
-void startWifi() 
-{
-  //esp modülü ile bağlantıyı kurabilmişsek modül "AT" komutuna "OK" komutu ile geri dönüş yapıyor.
-  Serial.println("AT+CWMODE=3"); //esp modülümüzün WiFi modunu STA şekline getiriyoruz. Bu mod ile modülümüz başka ağlara bağlanabilecek.
-  delay(2000);
-  String baglantiKomutu = "AT+CWJAP=\"";
-  baglantiKomutu += + ag_ismi;
-  baglantiKomutu += "\",\"";
-  baglantiKomutu += ag_sifresi;
-  baglantiKomutu += "\"";
-
-  Serial.println(baglantiKomutu);
-  delay(5000);
-
-  if (Serial.find("OK"))
-  {
-    return true;
-  }
-  else
-  {
-    return false;
-  }
-}
-
-
-void gelenVeri() 
-{
-  String gelen = "", jsonData = "";
-
-  if (Serial.available() > 0) 
-  {
-    gelen = Serial.readString();
-  }
-
-  Serial.println("---------GELEN Veri: " + String(sayi) + "-----------");
-  Serial.println(String(gelen));
-  Serial.println();
-
-  jsonData = parseJsonStr(gelen);
-  Serial.println("---------JSON Parsed-----------");
-  Serial.println(jsonData);
-  Serial.println();
-
-
-  StaticJsonDocument<1000> doc;
-  //deserializeJson(doc , jsonData);
-
-  DeserializationError error = deserializeJson(doc, jsonData);
-
-  // Test if parsing succeeds.
-  if (error) 
-  {
-    Serial.print(F("deserializeJson() failed: "));
-    Serial.println(error.c_str());
-    return;
-  }
-
-  // device init
-  JsonObject feeds_0 = doc["feeds"][0];
-
-  const char* feeds_0_created_at = feeds_0["created_at"]; // "2019-09-30T10:52:56Z"
-  int feeds_0_entry_id = feeds_0["entry_id"]; // 112
-  const char* feeds_0_field1 = feeds_0["field1"]; // "1"
-
-  Serial.println("---------JSON Response-----------");
-
-  Serial.print("created_at: ");
-  Serial.println(feeds_0_created_at);
-  Serial.print("entry_id: ");
-  Serial.println(feeds_0_entry_id);
-  Serial.print("field1: ");
-  Serial.println(feeds_0_field1);
-}
-
-String parseJsonStr(String gelen) 
-{
-  int find1 = 0, find2 = 0;
-  String findChar1 = "{", findChar2 = "}";
-  String JsonStr = "";
-
-  find1 = gelen.indexOf(findChar1);
-  find2 = gelen.lastIndexOf(findChar2);
-
-  JsonStr = gelen.substring(find1, find2 + 1);
-  return JsonStr;
+  delay(1000);
+  Serial.println(birlesmis);
+  Serial.println(sizeof(birlesmis));
 }
---
+++ Projeler/WIFI/WIFI_ThingSpeak_Get_Nano/WIFI_ThingSpeak_Get_Nano.ino
@@ -0,0 +1,162 @@
+//ESP TX >> Mega RX(0)
+//ESP RX >> Mega TX(0)
+
+#include <ArduinoJson.h>
+
+char ag_ismi[5] = "KAT3";
+char ag_sifresi[14] = "UnV-2019!Wf++";
+// const ag_ismi[14] = "SONRASI_YOKTU";
+// #define ag_sifresi[14] = "BuuRA03045025";
+
+
+char host[16] = "184.106.153.149";
+int sayi = 0;
+
+void setup()
+{
+  Serial.begin(115200); //Seriport'u açıyoruz. Güncellediğimiz
+
+  Serial.println("AT");
+  delay(5000);
+
+  if (Serial.find("OK"))
+  {
+    //connect to your wifi netowork
+    startWifi();
+  } 
+}
+
+void loop() 
+{
+  httpRequest();
+  delay(10000);
+}
+
+void httpRequest() 
+{
+  char start_sentence[44] = "AT+CIPSTART=\"TCP\",\"";
+  strcat(start_sentence, host);
+  strcat(start_sentence, "\",80");
+
+  Serial.println(start_sentence);  //AT+CIPSTART komutu ile sunucuya bağlanmak için sunucudan izin istiyoruz.
+  delay(2000);
+
+  if (Serial.find("Error"))
+  {
+    return false;
+  }
+
+  sayi++;
+  char postData[116] = "GET /channels/871881/feeds.json?results=1 HTTP/1.1\r\n";
+  strcat(postData, "Host: api.thingspeak.com\r\n");
+  strcat(postData, "Connection: keep-alive\r\n");
+  strcat(postData, "\r\n");
+
+  //TCP burada yapacağımız bağlantı çeşidini gösteriyor. 80 ise bağlanacağımız portu gösteriyor
+
+  //veri yollayacağımız zaman bu komutu kullanıyoruz. Bu komut ile önce kaç tane karakter yollayacağımızı söylememiz gerekiyor.
+  Serial.print("AT+CIPSEND="); 
+  Serial.println(sizeof(postData));
+
+  //eğer sunucu ile iletişim sağlayıp komut uzunluğunu gönderebilmişsek ESP modülü bize ">" işareti ile geri dönüyor.
+  if (Serial.find(">")) {
+        // arduino da ">" işaretini gördüğü anda sıcaklık verisini esp modülü ile thingspeak sunucusuna yolluyor.
+        Serial.print(postData);
+        // delay(2000); //Delayi önce koyunca veriyi çekemedi
+        // gelenVeri();
+        delay(3000);
+        Serial.println("AT+CIPCLOSE");
+
+        // çalışma sıklığı saniye olarak
+    //delay(device.interval * 1000);
+        delay(2500);
+
+  }
+}
+void startWifi() 
+{
+  //esp modülü ile bağlantıyı kurabilmişsek modül "AT" komutuna "OK" komutu ile geri dönüş yapıyor.
+  Serial.println("AT+CWMODE=3"); //esp modülümüzün WiFi modunu STA şekline getiriyoruz. Bu mod ile modülümüz başka ağlara bağlanabilecek.
+  delay(2000);
+
+  char baglantiKomutu[40] = "AT+CWJAP=\"";
+  strcat(baglantiKomutu, ag_ismi);
+  strcat(baglantiKomutu, "\",\"");
+  strcat(baglantiKomutu, ag_sifresi);
+  strcat(baglantiKomutu, "\"");
+
+  Serial.println(baglantiKomutu);
+  delay(5000);
+
+  if (Serial.find("OK"))
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+
+
+// void gelenVeri() 
+// {
+//   const size_t capacity = JSON_ARRAY_SIZE(1) + JSON_OBJECT_SIZE(2) + JSON_OBJECT_SIZE(3) + JSON_OBJECT_SIZE(8) + 240;
+//   DynamicJsonDocument doc(capacity);
+
+//   char* gelen[] = "";
+//   char* jsonData[] = "";
+
+//   if (Serial.available() > 0) 
+//   {
+//     gelen = Serial.readString();
+//   }
+
+//   Serial.println("---------GELEN Veri: -----------");
+//   Serial.println(gelen);
+//   Serial.println();
+
+//   jsonData = parseJsonStr(gelen);
+//   Serial.println("---------JSON Parsed-----------");
+//   Serial.println(jsonData);
+//   Serial.println();
+
+//   DeserializationError error = deserializeJson(doc, jsonData);
+
+//   // Test if parsing succeeds.
+//   if (error) 
+//   {
+//     Serial.print(F("deserializeJson() failed: "));
+//     Serial.println(error.c_str());
+//     return;
+//   }
+
+//   // device init
+//   JsonObject feeds_0 = doc["feeds"][0];
+
+//   const char* feeds_0_created_at = feeds_0["created_at"]; // "2019-09-30T10:52:56Z"
+//   int feeds_0_entry_id = feeds_0["entry_id"]; // 112
+//   const char* feeds_0_field1 = feeds_0["field1"]; // "1"
+
+//   Serial.println("---------JSON Response-----------");
+
+//   Serial.print("created_at: ");
+//   Serial.println(feeds_0_created_at);
+//   Serial.print("entry_id: ");
+//   Serial.println(feeds_0_entry_id);
+//   Serial.print("field1: ");
+//   Serial.println(feeds_0_field1);
+// }
+
+// String parseJsonStr(String gelen) 
+// {
+//   int find1 = 0, find2 = 0;
+//   String findChar1 = "{", findChar2 = "}";
+//   String JsonStr = "";
+
+//   find1 = gelen.indexOf(findChar1);
+//   find2 = gelen.lastIndexOf(findChar2);
+
+//   JsonStr = gelen.substring(find1, find2 + 1);
+//   return JsonStr;
+// }
