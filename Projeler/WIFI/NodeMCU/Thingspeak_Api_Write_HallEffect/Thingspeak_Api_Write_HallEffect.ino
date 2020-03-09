98a1f2dc63396e49bafd4d4ffb7d3a2c1fc21be0
bugrabugra <bugraotken@gmail.com>
2020-03-09 08:58:39 +0300

    Node

--- Projeler/WIFI/NodeMCU/Thingspeak_Api_Write_HallEffect/Thingspeak_Api_Write_HallEffect.ino
+++ Projeler/WIFI/NodeMCU/Thingspeak_Api_Write_HallEffect/Thingspeak_Api_Write_HallEffect.ino
@@ -6,10 +6,9 @@
 const char ssid[] = WIFI_SSID_EV; //Your Network SSID
 const char pass[] = WIFI_PASSWORD_EV; //Your Network Password
 
-int pinButton = 2; //*******************
-int valButton; //*******************
-// int val;
-// int pinHallEffect = 3;
+int pinHallEffect = 4;
+int pinLED = 13;
+int valHallEffect;
 
 WiFiClient client;
 
@@ -18,10 +17,11 @@ const char *myWriteAPIKey = THINGSPEAK_1009245_W; //Your Write API Key
 
 void setup()
 {
-  pinMode(pinButton, INPUT_PULLUP);
-
   Serial.begin(9600);
   delay(10);
+
+  pinMode(pinHallEffect, INPUT);
+  pinMode(pinLED, OUTPUT);
 
   // Connect to WiFi network
   WiFi.begin(WIFI_SSID_EV, WIFI_PASSWORD_EV);
@@ -31,15 +31,17 @@ void setup()
 
 void loop()
 {
-  valButton = digitalRead(pinButton); //*******************
-  // val = digitalRead(pinHallEffect);
+  valHallEffect = digitalRead(pinHallEffect);
+  Serial.print(valHallEffect);
 
-  // Serial.print(val);
-  Serial.print(valButton);
-
-  delay(1000);
-  
-  ThingSpeak.writeField(myChannelNumber, 1,valButton, myWriteAPIKey); //*******************
-  // ThingSpeak.writeField(myChannelNumber, 1,val, myWriteAPIKey); //Update in ThingSpeak
-  delay(5000);
+  if (valHallEffect == 0)
+  {
+    ThingSpeak.writeField(myChannelNumber, 1, valHallEffect, myWriteAPIKey);
+    digitalWrite(pinLED, HIGH);
+    delay(17000);
+  }
+  else
+  {
+    digitalWrite(pinLED, LOW);
+  }
 }
