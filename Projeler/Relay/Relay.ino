4828af9b1ea4ed7fe3d0ea5c0d40a5ab6e647930
bugrabugra <bugraotken@gmail.com>
2020-03-10 23:12:28 +0300

    Relay

---
+++ Projeler/Relay/Relay.ino
@@ -0,0 +1,37 @@
+int pinButton = 3;
+int pinRelay1 = 6;
+int pinRelay2 = 7;
+bool stateButton;
+
+void setup() 
+{
+  Serial.begin(9600);
+  pinMode(pinButton, INPUT_PULLUP);
+  pinMode(pinRelay1, OUTPUT);
+  pinMode(pinRelay2, OUTPUT);
+  stateButton = false;
+}
+
+
+void loop()
+{
+  if (digitalRead(pinButton) == LOW)
+  {
+    if (stateButton == false)
+    {
+      stateButton = true;
+      digitalWrite(pinRelay1, HIGH);
+      digitalWrite(pinRelay2, HIGH);
+      Serial.println("On");
+      delay(500);
+    }
+    else
+    {
+      stateButton = false;
+      digitalWrite(pinRelay1, LOW);
+      digitalWrite(pinRelay2, LOW);
+      Serial.println("Off");
+      delay(500);
+    } 
+  }
+}
