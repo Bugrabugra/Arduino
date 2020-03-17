eb91f269568dcb2e762cad400953f7604bc7a7c4
bugrabugra <bugraotken@gmail.com>
2020-03-17 18:45:22 +0300

    NodeMCU

---
+++ Projeler/WIFI/NodeMCU/Host_Client/Client/Client.ino
@@ -0,0 +1,98 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
+
+#include <ESP8266WiFiMulti.h>
+ESP8266WiFiMulti WiFiMulti;
+
+int pinLED = 4;
+
+const char* ssid = "ESP8266-Access-Point";
+const char* password = "123456789";
+
+//Your IP address or domain name with URL path
+const char* serverLED = "http://192.168.4.1/LED";
+
+String stateButton;
+
+unsigned long previousMillis = 0;
+const long interval = 1000; 
+
+void setup() 
+{
+  Serial.begin(115200);
+  pinMode(pinLED, OUTPUT);
+  Serial.println();
+  
+  WiFi.mode(WIFI_STA);
+  WiFiMulti.addAP(ssid, password);
+  while((WiFiMulti.run() == WL_CONNECTED)) 
+  { 
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("Connected to WiFi");
+}
+
+void loop() 
+{
+  unsigned long currentMillis = millis();
+  
+  if(currentMillis - previousMillis >= interval) 
+  {
+     // Check WiFi connection status
+    if ((WiFiMulti.run() == WL_CONNECTED)) 
+    {
+      stateButton = httpGETRequest(serverLED);
+      
+      Serial.println("State: " + stateButton);
+
+      if (stateButton == "1")
+      {
+        digitalWrite(pinLED, HIGH);
+      }
+      else
+      {
+        digitalWrite(pinLED, LOW);
+      }
+   
+      // save the last HTTP GET Request
+      previousMillis = currentMillis;
+    }
+    else 
+    {
+      Serial.println("WiFi Disconnected");
+    }
+  }
+}
+
+String httpGETRequest(const char* serverName) 
+{
+  WiFiClient client;
+  HTTPClient http;
+    
+  // Your IP address with path or Domain name with URL path 
+  http.begin(client, serverName);
+  
+  // Send HTTP POST request
+  int httpResponseCode = http.GET();
+  
+  String payload = "--"; 
+  
+  if (httpResponseCode>0) 
+  {
+    Serial.print("HTTP Response code: ");
+    Serial.println(httpResponseCode);
+    payload = http.getString();
+  }
+  else 
+  {
+    Serial.print("Error code: ");
+    Serial.println(httpResponseCode);
+  }
+  // Free resources
+  http.end();
+
+  return payload;
+}
