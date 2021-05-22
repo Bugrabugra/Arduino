0a69b3afbe222feab62a85ab37e365029541c00c
bugrabugra <bugraotken@gmail.com>
2021-05-22 17:25:19 +0300

    Aliexpress

--- Projeler/TFT_Screen_ST7735/TFT_Screen_ST7735.ino
+++ Projeler/TFT_Screen_ST7735/TFT_Screen_ST7735.ino
@@ -1,9 +1,52 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Adafruit_GFX.h>    // Core graphics library
+#include <Adafruit_ST7735.h> // Hardware-specific library
+#include <SPI.h>
+ 
+//Arduino Uno/Nano Pins:
+//GND = GND
+//VCC = 5v
+//DC = 8
+//RES = 9
+//CS = 10
+//SDA = 11
+//SCL = 13
+ 
+#define TFT_CS     10
+#define TFT_RST    9
+#define TFT_DC     8
+Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
 
+void setup(void) {
+  tft.initR(INITR_REDTAB);
+  tft.setRotation(3);
+
+  //tft.initR(INITR_MINI160x80); //Initializes ST7735.
+  //Please note that use of 'MINI160x80' displays cyan instead of yellow and vice versa.
+ 
+  //tft.setRotation(3);
+  tft.fillScreen(ST7735_BLACK);
+ 
+  tft.setCursor(5, 5);
+  tft.setTextColor(ST7735_RED); 
+  tft.setTextSize(1);
+  tft.println("Welcome to");
+  tft.setCursor(5, 20);
+  tft.setTextColor(ST7735_GREEN);
+  tft.setTextSize(2);
+  tft.println("ezContents");
+  tft.setCursor(5, 38);
+  tft.setTextColor(ST7735_BLUE);
+  tft.setTextSize(3);
+  tft.print("Blogs");
+ 
+  tft.drawLine(153, 0, 153, 79, ST7735_WHITE);
+  tft.drawCircle(153, 20, 5, ST7735_CYAN);
+  tft.fillTriangle(153, 35, 148, 45, 158, 45, ST7735_MAGENTA);
+  tft.drawTriangle(153, 35, 148, 45, 158, 45, ST7735_WHITE);
+  tft.fillCircle(153, 60, 5, ST7735_YELLOW);
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
+ 
+void loop()
+{
+ 
 }
